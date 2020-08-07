f2ac63feee41132e2f667bbda1e588a193a523d3
DenFade <rurchi1206@gmail.com>
2020-08-07 20:57:56 +0900

    Beta 1.3 (Additional)

--- app/src/main/java/com/github/DenFade/autohcheck/MainActivity.java
+++ app/src/main/java/com/github/DenFade/autohcheck/MainActivity.java
@@ -2,29 +2,32 @@ package com.github.DenFade.autohcheck;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
+import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView title;
-    private Button onoff_btn;
+    private Button on_off_btn;
     private Button setting_btn;
+    private boolean setBlinking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +35,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         title = (TextView) findViewById(R.id.app_title);
-        onoff_btn = (Button) findViewById(R.id.app_onoff);
+        on_off_btn = (Button) findViewById(R.id.app_onoff);
         setting_btn = (Button) findViewById(R.id.app_setting);
 
-        onoff_btn.setText(getColoredOnOffText(HcheckService.isRunning()));
-        onoff_btn.setOnClickListener(new View.OnClickListener() {
+        //title 애니메이션 설정
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        setBlinking = pref.getBoolean("blink", false);
+
+        if(setBlinking){
+            Animation animation = new AlphaAnimation(1f, 0.3f);
+            animation.setDuration(1500);
+            animation.setInterpolator(new LinearInterpolator());
+            animation.setRepeatCount(Animation.INFINITE);
+            animation.setRepeatMode(Animation.REVERSE);
+            title.startAnimation(animation);
+        }
+
+        //버튼 설정
+        on_off_btn.setText(getColoredOnOffText(HcheckService.isRunning()));
+        on_off_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, HcheckService.class);
                 if(HcheckService.isRunning()){
-                    onoff_btn.setText(getColoredOnOffText(false));
+                    on_off_btn.setText(getColoredOnOffText(false));
                     stopService(intent);
                 } else {
                     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                        onoff_btn.setText(getColoredOnOffText(true));
+                        on_off_btn.setText(getColoredOnOffText(true));
                         startForegroundService(intent);
                     } else {
                         startService(intent);
@@ -66,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) createNotificationChannel();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //애니메이션 해제
+        if(setBlinking) title.clearAnimation();
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void createNotificationChannel() {
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
@@ -79,15 +104,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private CharSequence getColoredOnOffText(boolean isRun){
-        SpannableString s = new SpannableString(onoff_btn.getText());
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            if (isRun) {
-                s.setSpan(new ForegroundColorSpan(getColor(R.color.service_on)), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                s.setSpan(new ForegroundColorSpan(Color.BLACK), 5, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            } else {
-                s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                s.setSpan(new ForegroundColorSpan(getColor(R.color.service_off)), 5, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
+        SpannableString s = new SpannableString(on_off_btn.getText());
+        if (isRun) {
+            s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(this, R.color.service_on)), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            s.setSpan(new ForegroundColorSpan(Color.BLACK), 5, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        } else {
+            s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            s.setSpan(new ForegroundColorSpan( ContextCompat.getColor(this, R.color.service_off)), 5, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
         return s;
     }
